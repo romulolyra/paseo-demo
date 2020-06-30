6cee2007ab16ad5a74bf5ae4629b283777458392
romulolyra <65560137+romulolyra@users.noreply.github.com>
2020-06-30 11:50:35 -0300

    Imagem logo

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # paseo-demo
- site caronas
+
+Segunda versão do site: 
+
+Páginas disponíveis: (localhost)/login : Tela inicial 
+                      (localhost)/cadastro: Tela de cadastro
+                      (localhost)/sobre-nos: Tela simples explicando o projeto
+                      (localhost)/inicial: Tela que mostraria as informações dos demais usuário(incompleta)
--- paseo/app/models.py
+++ paseo/app/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 import requests
-
+#modelo de usuário da primeira versão:
 class User(models.Model):
 
     CURSOS = (
@@ -16,94 +16,102 @@ class User(models.Model):
         max_length= 25,
         choices=CURSOS,
     )
-
+#modelo de usuário da segunda versão:
 class NovoUser(models.Model):
-
-
-
-
     def __str__(self):
         return self.nome
     CURSOS = (
-        ('cc','Ciências da computação'),
-        ('ds','Design'),
+        ('Ciências da computação','Ciências da computação'),
+        ('Design','Design'),
     )
+
+    #funçao que busca informações da localização da pessoa pelo CEP a partir da API ViaCEP
+
+    def via_cep(cep):
+        info = requests.get("https://viacep.com.br/ws/" + str(cep) + "/json/")
+        return (info.json()['bairro'])
+
+
+
     nome = models.CharField(max_length=100)
     sobrenome = models.CharField(max_length=100)
-    foto = models.ImageField()
+    foto = models.ImageField() #Framework pode pedir uma extenção adicinal para lidar com arquivos estáticos
     email = models.EmailField()
     senha1 = models.CharField(max_length=50)
-    senha2= models.CharField(max_length=50)
+    senha2 = models.CharField(max_length=50)
     largadouro = models.CharField(max_length=200)
-    numero =  models.CharField(max_length=10)
+    numero = models.CharField(max_length=10)
     cep = models.CharField(max_length=10)
     curso = models.CharField(
-        max_length= 25,
+        max_length=25,
         choices=CURSOS,
     )
-    #*** carro ***
-    placa = models.CharField(max_length=9)
-    cor = models.CharField(max_length=19)
-    modelo =  models.CharField(max_length=19)
-    #-- indices reservas caso deseje adicionar algum sem mudar a estrutura de dados:
-    reserva1 = models.CharField(max_length=50)
-    reserva2 = models.CharField(max_length=50)
+    # *** carro ***
+    #foi necessário indicar valores padrões para caso o usuário não digite nenhuma opção
+    placa = models.CharField(max_length=9, null=True, blank=True)
+    cor = models.CharField(max_length=19, null=True, blank=True)
+    modelo = models.CharField(max_length=19, null=True, blank=True)
+    # -- indices reservas caso deseje adicionar algum sem mudar a estrutura de dados:
+    reserva1 = models.CharField(max_length=50, null=True, blank=True)
+    reserva2 = models.CharField(max_length=50, null=True, blank=True)
 
     # **** interesses: *****
 
     #  esportes
-    e1 = models.BooleanField(default=False)
-    e2 = models.BooleanField(default=False)
-    e3 = models.BooleanField(default=False)
-    e4 = models.BooleanField(default=False)
+    e1 = models.BooleanField(default=False, blank=False)
+    e2 = models.BooleanField(default=False, blank=False)
+    e3 = models.BooleanField(default=False, blank=False)
+    e4 = models.BooleanField(default=False, blank=False)
 
     # reality shows
-    r1 = models.BooleanField(default=False)
-    r2 = models.BooleanField(default=False)
-    r3 = models.BooleanField(default=False)
-    r4 = models.BooleanField(default=False)
+    r1 = models.BooleanField(default=False, blank=False)
+    r2 = models.BooleanField(default=False, blank=False)
+    r3 = models.BooleanField(default=False, blank=False)
+    r4 = models.BooleanField(default=False, blank=False)
 
     #  jogos
-    j1 = models.BooleanField(default=False)
-    j2 = models.BooleanField(default=False)
-    j3 = models.BooleanField(default=False)
-    j4 = models.BooleanField(default=False)
-    j5 = models.BooleanField(default=False)
-    j6 = models.BooleanField(default=False)
+    j1 = models.BooleanField(default=False, blank=False)
+    j2 = models.BooleanField(default=False, blank=False)
+    j3 = models.BooleanField(default=False, blank=False)
+    j4 = models.BooleanField(default=False, blank=False)
+    j5 = models.BooleanField(default=False, blank=False)
+    j6 = models.BooleanField(default=False, blank=False)
 
     # animes
-    a1 = models.BooleanField(default=False)
-    a2 = models.BooleanField(default=False)
-    a3 = models.BooleanField(default=False)
-    a4 = models.BooleanField(default=False)
-    a5 = models.BooleanField(default=False)
-    a6 = models.BooleanField(default=False)
+    a1 = models.BooleanField(default=False, blank=False)
+    a2 = models.BooleanField(default=False, blank=False)
+    a3 = models.BooleanField(default=False, blank=False)
+    a4 = models.BooleanField(default=False, blank=False)
+    a5 = models.BooleanField(default=False, blank=False)
+    a6 = models.BooleanField(default=False, blank=False)
 
     # streaming
-    s1 = models.BooleanField(default=False)
-    s2 = models.BooleanField(default=False)
-    s3 = models.BooleanField(default=False)
-    s4 = models.BooleanField(default=False)
+    s1 = models.BooleanField(default=False, blank=False)
+    s2 = models.BooleanField(default=False, blank=False)
+    s3 = models.BooleanField(default=False, blank=False)
+    s4 = models.BooleanField(default=False, blank=False)
 
-    #hobbies
-    h1 = models.BooleanField(default=False)
-    h2 = models.BooleanField(default=False)
-    h3 = models.BooleanField(default=False)
-    h4 = models.BooleanField(default=False)
-    h5 = models.BooleanField(default=False)
-    h6 = models.BooleanField(default=False)
+    # hobbies
+    h1 = models.BooleanField(default=False, blank=False)
+    h2 = models.BooleanField(default=False, blank=False)
+    h3 = models.BooleanField(default=False, blank=False)
+    h4 = models.BooleanField(default=False, blank=False)
+    h5 = models.BooleanField(default=False, blank=False)
+    h6 = models.BooleanField(default=False, blank=False)
 
-    #sobre voce
-    so1 = models.BooleanField(default=False)
-    so2 = models.BooleanField(default=False)
-    so3 = models.BooleanField(default=False)
-    so4 = models.BooleanField(default=False)
+    # sobre voce
+    so1 = models.BooleanField(default=False, blank=False)
+    so2 = models.BooleanField(default=False, blank=False)
+    so3 = models.BooleanField(default=False, blank=False)
+    so4 = models.BooleanField(default=False, blank=False)
 
-    #categoria vazia caso seja preciso adicionar mais uma
-    v1 = models.BooleanField(default=False)
-    v2 = models.BooleanField(default=False)
-    v3 = models.BooleanField(default=False)
-    v4 = models.BooleanField(default=False)
-    v5 = models.BooleanField(default=False)
-    v6 = models.BooleanField(default=False)
+    # categoria vazia caso seja preciso adicionar mais uma
+    v1 = models.BooleanField(default=False, blank=False)
+    v2 = models.BooleanField(default=False, blank=False)
+    v3 = models.BooleanField(default=False, blank=False)
+    v4 = models.BooleanField(default=False, blank=False)
+    v5 = models.BooleanField(default=False, blank=False)
+    v6 = models.BooleanField(default=False, blank=False)
+
+
 
